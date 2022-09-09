25759d6b48d8f4f0af21f1120bc298abd4311e2c
drgomesp <drgomesp@gmail.com>
2022-09-08 21:45:07 -0300

    removed unused code

--- examples/hostinfo.go
+++ examples/hostinfo.go
@@ -2,41 +2,13 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
-	"fmt"
-	"io"
 	"log"
-	mr "math/rand"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/libp2p/go-libp2p/core/host"
 
 	hostinfo "github.com/drgomesp/go-libp2p-hostinfo"
 )
-
-func makeBasicHost(listenPort int, randseed int64) (host.Host, error) {
-	var r io.Reader
-	if randseed == 0 {
-		r = rand.Reader
-	} else {
-		r = mr.New(mr.NewSource(randseed))
-	}
-
-	privk, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
-	check(err)
-
-	opts := []libp2p.Option{
-		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", listenPort)),
-		libp2p.Identity(privk),
-		libp2p.DisableRelay(),
-	}
-
-	opts = append(opts, libp2p.NoSecurity)
-
-	return libp2p.New(opts...)
-}
 
 func main() {
 	host, err := libp2p.New()
@@ -61,10 +33,4 @@ func main() {
 	log.Println("visit: http://localhost:4000/v1/hostinfo")
 
 	<-ctx.Done()
-}
-
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
